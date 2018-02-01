a6624db3259b57b726fbd55959da0ce3cc84c73e
husky-parul <promptparul@gmail.com>
2018-02-01 12:40:24 -0500

    app.py added with requirements

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask
+from flask import request
+
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def index():
+    return "Python Flask server running on OpenShift.."
+
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
