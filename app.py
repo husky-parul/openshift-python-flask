788e72b3909614621b62346febd3ee5724fa977b
husky-parul <singh.p@husky.neu.edu>
2018-03-09 10:15:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,22 @@ def main():
 
 if __name__ == "__main__":
     print ("This is point 1")
+    scale = 2
+    print ("We are inside main")
+    spark = SparkSession.builder.appName("PythonPi").getOrCreate()
+    n = 100000 * scale
+    print ("Value of n: ",n)
+    def f(_):
+        from random import random
+        x = random()
+        y = random()
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+    count = spark.sparkContext.parallelize(
+        xrange(1, n + 1), scale).map(f).reduce(lambda x, y: x + y)
+    spark.stop()
+    pi = 4.0 * count / n
+    print ("Value of pi: ", pi)
+    print ("********* this is the end ****************")
+    
     #main()
