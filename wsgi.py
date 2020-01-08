1be0d64e6a6f24a5ebdcdf92a7beb74ef7d953be
husky-parul <parsingh@redhat.com>
2020-01-08 07:00:35 -0500

    lot of cpu workload

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,18 @@ def hello():
     # return "Hello World Parul!"
     return jsonify("Hello World Parul!")
 
+@application.route("/cpu")
+def lotsofcpu():
+    # do a lot of cpu work
+    arr = [0] * 20000000
+    sum = 1
+    for i in range(len(arr)):
+        sum+=arr[i]
+    
+    data = "sum " + str(sum)
+    return jsonify(data)
+    
+
+
 if __name__ == "__main__":
     application.run()
