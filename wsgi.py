8e062b58d51b449b243d67d2f7400ae4031f698d
husky-parul <parsingh@redhat.com>
2020-01-08 06:39:17 -0500

    return json

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import jsonify
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World Parul!"
+    # return "Hello World Parul!"
+    return jsonify("Hello World Parul!")
 
 if __name__ == "__main__":
     application.run()
